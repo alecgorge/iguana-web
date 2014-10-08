734af1b23df7cf92b271c8f35226d6a800fada61
Kirby Kohlmorgen <kirby@purdue.edu>
2014-10-08 01:02:14 -0700

    Play functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,42 @@
 $(function() {
   $('#sign-in-modal').modal();
 });
+
+
+$('#magic').click(function() {
+  console.log("hi");
+  window.location = '/loggedin.html';
+});
+
+
+$('#magic2').click(function() {
+  console.log("hi");
+  window.location = '/loggedin.html';
+});
+
+var audio;
+var first = true;
+
+$('#play').click(function() {
+  if(first) {
+    audio = new Audio('/music.mp3');
+    first = false;
+  }
+  audio.play();
+  $('#pause').toggle();
+  $('#play').toggle();
+});
+
+$('#pause').click(function() {
+  $('#pause').toggle();
+  $('#play').toggle();
+  audio.pause();
+});
+
+$('#fast-forward').click(function() {
+  audio.pause();
+});
+
+$('#fast-backward').click(function() {
+  audio.pause();
+});

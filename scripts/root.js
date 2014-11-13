b48c33624ad2b567fee32be9d3d84ffb526af914
Kirby Kohlmorgen <kirby@purdue.edu>
2014-11-13 16:00:42 -0500

    Kinda refactor resourcing functions

--- scripts/root.js
+++ scripts/root.js
@@ -6,28 +6,47 @@ var root = angular.module('root', ["ngResource"])
   var shows = $resource('http://localhost:9000/api/artists/:artist_slug/years/:year_slug');
   var recordings = $resource('http://localhost:9000/api/artists/:artist_slug/years/:year_slug/shows/:show_date');
 
+  var current_artist;
+  var current_year;
+
   artists.get().$promise.then(function(result) {
     $scope.artists = result.data;
   });
 
   $scope.getYears = function(artist) {
     years.get({artist_slug: artist.id}).$promise.then(function(result) {
+      current_artist = artist;
       $scope.years = result.data;
     });
-  }
+  };
 
-  $scope.getShows = function(artist, year) {
-    shows.get({artist_slug: artist, year_slug: year}).$promise.then(function(result) {
+  $scope.getShows = function(year) {
+    shows.get({artist_slug: current_artist.id, year_slug: year.year}).$promise.then(function(result) {
+      current_year = year;
       $scope.shows = result.data.shows;
     });
-  }
+  };
 
-  $scope.getRecordings = function(artist, year, show) {
-    recordings.get({artist_slug: artist, year_slug: year, show_date: show}).$promise.then(function(result) {
-      console.log(result);
+  $scope.getRecordings = function(show) {
+    recordings.get({artist_slug: current_artist.id, year_slug: current_year.year, show_date: show.display_date}).$promise.then(function(result) {
       $scope.recordings = result.data[0].tracks;
     });
-  }
+  };
+
+  $scope.playSong = function(recording) {
+    $scope.title = recording.title;
+    $scope.artist = current_artist.name;
+
+    var length = recording.length;
+    var min = Math.floor(length/60);
+    var sec = pad(length % 60, 2);
+    $scope.total_time = min + ':' + sec;
+  };
 
 }]);
 
+function pad(n, width, z) {
+  z = z || '0';
+  n = n + '';
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
